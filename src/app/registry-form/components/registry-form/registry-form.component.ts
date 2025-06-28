0d54c1c4bea9d22a0038f87c831cf1cc22dc2439
VHMI <vhmizq.com>
2025-06-27 21:37:36 -0600

    form components creation

---
+++ src/app/registry-form/components/registry-form/registry-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lra-registry-form',
+  imports: [],
+  templateUrl: './registry-form.component.html',
+  styleUrl: './registry-form.component.scss'
+})
+export class RegistryFormComponent {
+
+}
---
+++ src/app/registry-form/views/registry-form-view/registry-form-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lra-registry-form-view',
+  imports: [],
+  templateUrl: './registry-form-view.component.html',
+  styleUrl: './registry-form-view.component.scss'
+})
+export class RegistryFormViewComponent {
+
+}
