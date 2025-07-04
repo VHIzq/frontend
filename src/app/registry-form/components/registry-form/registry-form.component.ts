0f1430bea4c2f06a5c8b0eef6fbfcb00f993f138
VHMI <vhmizq.com>
2025-07-03 21:27:29 -0600

    add form

--- src/app/registry-form/components/registry-form/registry-form.component.ts
+++ src/app/registry-form/components/registry-form/registry-form.component.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RegistryForm } from './registry-form.model';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'lra-registry-form',
-  imports: [],
+  imports: [
+    // Angular
+    ReactiveFormsModule,
+
+    // Material
+    MatInputModule,
+  ],
   templateUrl: './registry-form.component.html',
-  styleUrl: './registry-form.component.scss'
+  styleUrl: './registry-form.component.scss',
 })
 export class RegistryFormComponent {
+  registryForm!: FormGroup;
 
+  private fb = inject(FormBuilder);
+
+  private setupRegistryForm() {
+    this.registryForm = this.fb.group({
+      name: this.fb.control(null),
+      firstLastName: this.fb.control(null),
+      secondsLastName: this.fb.control(null),
+      age: this.fb.control(null),
+      email: this.fb.control(null),
+      cellphone: this.fb.control(null),
+      street: this.fb.control(null),
+      streetNumber: this.fb.control(null),
+      zipCode: this.fb.control(null),
+      referende: this.fb.control(null),
+      region: this.fb.control(null),
+      state: this.fb.control(null),
+      houseNumber: this.fb.control(null),
+      sex: this.fb.control(null),
+      peaceHouseLeader: this.fb.control(null),
+      peaceHouseNumber: this.fb.control(null),
+      isFirstTimeVisit: this.fb.control(null),
+      dateFirstTimeVisit: this.fb.control(Date)
+    });
+  }
 }
---
+++ src/app/registry-form/components/registry-form/registry-form.model.ts
@@ -0,0 +1,21 @@
+export namespace RegistryForm {
+  export interface FormDataModel {
+  name?: string;
+  firstLastName?: string | null;
+  secondsLastName?: string | null;
+  age?: string | null;
+  email?: string | null;
+  cellphone?: string | null;
+  street?: string | null;
+  streetNumber?: string | null;
+  zipCode?: string | null;
+  referende?: string | null;
+  region?: string | null;
+  state?: string | null;
+  houseNumber?: string | null;
+  sex?: string | null;
+  peaceHouseLeader?: string | null;
+  peaceHouseNumber?: string | null;
+  firstTimeVisit?: Date | null;
+  }
+}
