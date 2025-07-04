564d02a338e990996435a0aa88961d9ecd9fbcc1
Victor Hugo <victorhugo@MacBook-Air-de-Victor-2.local>
2025-07-03 22:50:51 -0600

    add registry form

--- src/app/registry-form/components/registry-form/registry-form.component.ts
+++ src/app/registry-form/components/registry-form/registry-form.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import {
-  EmailValidator,
   FormBuilder,
-  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { RegistryForm } from './registry-form.model';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'lra-registry-form',
@@ -18,35 +19,51 @@ import { MatInputModule } from '@angular/material/input';
 
     // Material
     MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatIconModule,
   ],
   templateUrl: './registry-form.component.html',
   styleUrl: './registry-form.component.scss',
 })
-export class RegistryFormComponent {
+export class RegistryFormComponent implements OnInit {
   registryForm!: FormGroup;
 
   private fb = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.setupRegistryForm();
+  }
+
+  handleOnSubmit() {
+    if (this.registryForm.invalid) {
+      this.registryForm.markAllAsTouched();
+      return;
+    }
+
+    //Dipatch the form value to the store or service
+  }
+
   private setupRegistryForm() {
     this.registryForm = this.fb.group({
-      name: this.fb.control(null),
-      firstLastName: this.fb.control(null),
-      secondsLastName: this.fb.control(null),
-      age: this.fb.control(null),
-      email: this.fb.control(null),
-      cellphone: this.fb.control(null),
-      street: this.fb.control(null),
-      streetNumber: this.fb.control(null),
-      zipCode: this.fb.control(null),
-      referende: this.fb.control(null),
-      region: this.fb.control(null),
-      state: this.fb.control(null),
-      houseNumber: this.fb.control(null),
-      sex: this.fb.control(null),
-      peaceHouseLeader: this.fb.control(null),
-      peaceHouseNumber: this.fb.control(null),
-      isFirstTimeVisit: this.fb.control(null),
-      dateFirstTimeVisit: this.fb.control(Date)
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      firstLastName: ['', [Validators.required]],
+      secondsLastName: [''],
+      age: [null, [Validators.required, Validators.min(0)]],
+      email: ['', [Validators.required, Validators.email]],
+      cellphone: ['', [Validators.pattern(/^\d{10}$/)]],
+      street: [''],
+      streetNumber: [''],
+      zipCode: ['', [Validators.pattern(/^\d{5}$/)]],
+      referende: [''],
+      region: [''],
+      state: [''],
+      houseNumber: [''],
+      sex: ['', [Validators.required]],
+      peaceHouseLeader: [''],
+      peaceHouseNumber: [''],
+      isFirstTimeVisit: [null],
+      dateFirstTimeVisit: [null],
     });
   }
 }
