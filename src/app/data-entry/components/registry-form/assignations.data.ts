30f45612584f110d451552e9361f9e044986b6db
VHMI <vhmizq.com>
2025-08-01 14:50:20 -0600

    update mapping

--- src/app/data-entry/components/registry-form/assignations.data.ts
+++ src/app/data-entry/components/registry-form/assignations.data.ts
@@ -4,16 +4,59 @@ export const getNetworks = (): Array<string> => {
 
 export const getPastors = (): Record<string, Array<string>> => {
   return {
-    Isacar: ['Filiberto', 'Pastor 2'],
-    Sabaot: ['Rosita', 'Pastor 4'],
-    Gedeon: ['José', 'Pastor 6'],
+    Isacar: ['Anciano Filiberto Guzman'],
+    Sabaot: ['Anciano Moises Reyes', 'Anciana Rocio Reyes '],
+    Gedeon: ['Anciano Luis Ramirez'],
   };
 };
 
 export const getDiscipuladores = (): Record<string, Array<string>> => {
   return {
-    Isacar: ['Discipulador 1', 'Discipulador 2'],
-    Sabaot: ['Discipulador 3', 'Discipulador 4'],
-    Gedeon: ['Discipulador 5', 'Discipulador 6'],
+    Isacar: [
+      'Ana Laura Eleuterio',
+      'Concepcion Lopez',
+      'Esteban Segundo',
+      'Fernando Ramirez',
+      'Filiberto Guzman',
+      'Iris Alcaraz',
+      'Luz Magdaleno',
+      'Maria Gonzalez',
+      'Yanet Anaya',
+    ],
+    Sabaot: [
+      'Elia Collado Ibarra',
+      'Francisca Gálvez',
+      'Georgina Solorzano',
+      'Guillermina Solorzano',
+      'Juana Gálvez',
+      'Lilia Eugenio Andraca',
+      'Margarita de la Cruz',
+      'Rocío Gpe Escobar',
+      'Rosa María Blas',
+      'Rosa María Ferreira',
+      'Victor Hugo Ortiz Cruz',
+      'Yadira Rivas',
+    ],
+    Gedeon: [
+      'Arcelia Buendía Flores',
+      'Dora Elsa Montoya Sitalan',
+      'Guillermina Lauder',
+      'Isabel Rangel Romero',
+      'Ismerai Alvarado',
+      'Lourdes Jara López',
+      'Luis Ramírez Fuentes',
+      'Luz María Pérez',
+      'Malinal Colin',
+      'Marcelo Enriquez',
+      'Margarita de Jesús Padua',
+      'Margarita Salgado',
+      'Marisela Lobato Ojeda',
+      'Mauricio Sánchez',
+      'Nancy Cárdenas',
+      'Oscar Padua de Jesús',
+      'Patricia Palizada',
+      'Rosario Buendía',
+      'Rosario Ortiz',
+    ],
   };
 };
